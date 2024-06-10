d7c26181bb90224ea93099ab9a6ed52d33aca78d
Lucas Becker <lucasbecker.dev@gmail.com>
2024-06-10 17:02:24 -0400

    added @ManyToOne relationships with User and Channel

---
+++ src/main/java/com/example/coderscampus/assignment14/domain/Message.java
@@ -0,0 +1,19 @@
+package com.example.coderscampus.assignment14.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String content;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "channel_id")
+    private Channel channel;
+}
