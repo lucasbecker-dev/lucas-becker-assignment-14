5247ae781c01bd2774859ac5545a83b2efc9efa6
Lucas Becker <lucasbecker.dev@gmail.com>
2024-06-10 23:03:46 -0400

    added findByChannel() and save()

--- src/main/java/com/example/coderscampus/assignment14/service/MessageService.java
+++ src/main/java/com/example/coderscampus/assignment14/service/MessageService.java
@@ -1,8 +1,12 @@
 package com.example.coderscampus.assignment14.service;
 
+import com.example.coderscampus.assignment14.domain.Channel;
+import com.example.coderscampus.assignment14.domain.Message;
 import com.example.coderscampus.assignment14.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MessageService {
@@ -11,5 +15,13 @@ public class MessageService {
     @Autowired
     public MessageService(MessageRepository messageRepo) {
         this.messageRepo = messageRepo;
+    }
+
+    public List<Message> findByChannel(Channel channel) {
+        return messageRepo.findByChannel(channel);
+    }
+
+    public Message save(Message message) {
+        return messageRepo.save(message);
     }
 }
