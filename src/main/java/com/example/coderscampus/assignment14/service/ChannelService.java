ad852e6c54d26ba135e1d6b3b31192faeaef9339
Lucas Becker <lucasbecker.dev@gmail.com>
2024-06-10 20:23:31 -0400

    created ChannelService

---
+++ src/main/java/com/example/coderscampus/assignment14/service/ChannelService.java
@@ -0,0 +1,22 @@
+package com.example.coderscampus.assignment14.service;
+
+import com.example.coderscampus.assignment14.domain.Channel;
+import com.example.coderscampus.assignment14.repository.ChannelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ChannelService {
+    private final ChannelRepository channelRepo;
+
+    @Autowired
+    public ChannelService(ChannelRepository channelRepo) {
+        this.channelRepo = channelRepo;
+    }
+
+    public List<Channel> findAll() {
+        return channelRepo.findAll();
+    }
+}
