123d92273c6114ea163223b231e4a62bc72998e8
Lucas Becker <lucasbecker.dev@gmail.com>
2024-06-10 20:24:14 -0400

    created UserService

---
+++ src/main/java/com/example/coderscampus/assignment14/service/UserService.java
@@ -0,0 +1,20 @@
+package com.example.coderscampus.assignment14.service;
+
+import com.example.coderscampus.assignment14.domain.User;
+import com.example.coderscampus.assignment14.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepo;
+
+    @Autowired
+    public UserService(UserRepository userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    public void save(User user) {
+        userRepo.save(user);
+    }
+}
