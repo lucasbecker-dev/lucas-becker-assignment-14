5118dce9373ba9f5262979880cc50706feabcd5e
Lucas Becker <lucasbecker.dev@gmail.com>
2024-06-10 23:03:12 -0400

    added findByChannel()

--- src/main/java/com/example/coderscampus/assignment14/repository/MessageRepository.java
+++ src/main/java/com/example/coderscampus/assignment14/repository/MessageRepository.java
@@ -1,9 +1,13 @@
 package com.example.coderscampus.assignment14.repository;
 
+import com.example.coderscampus.assignment14.domain.Channel;
 import com.example.coderscampus.assignment14.domain.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
+    public List<Message> findByChannel(Channel channel);
 }
