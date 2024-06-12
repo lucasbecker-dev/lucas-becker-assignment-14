e4040d8e0d29af4a2013a7396f139072afb0ce41
Lucas Becker <lucasbecker.dev@gmail.com>
2024-06-12 01:15:03 -0400

    add /user GET endpoint

--- src/main/java/com/coderscampus/assignment14/rest/UserRestController.java
+++ src/main/java/com/coderscampus/assignment14/rest/UserRestController.java
@@ -3,6 +3,7 @@ package com.coderscampus.assignment14.rest;
 import com.coderscampus.assignment14.domain.User;
 import com.coderscampus.assignment14.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,16 @@ public class UserRestController {
     @Autowired
     public UserRestController(UserService userService) {
         this.userService = userService;
+    }
+
+    @GetMapping("/user")
+    public User getUser(@RequestBody String name) {
+        try {
+            return userService.findByName(name).get(0);
+        } catch (IndexOutOfBoundsException e) {
+            System.err.println("Error: no such User with name: " + name + "\n" + e);
+            return null;
+        }
     }
 
     @PostMapping("/user")
