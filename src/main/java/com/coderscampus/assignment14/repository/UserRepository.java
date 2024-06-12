c39a7577d85d189414ee6b90e49ff1048aebeeae
Lucas Becker <lucasbecker.dev@gmail.com>
2024-06-11 23:44:41 -0400

    added findByName()

--- src/main/java/com/coderscampus/assignment14/repository/UserRepository.java
+++ src/main/java/com/coderscampus/assignment14/repository/UserRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.coderscampus.assignment14.domain.User;
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    public List<User> findByName(String name);
 }
