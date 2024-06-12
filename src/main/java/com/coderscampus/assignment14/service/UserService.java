46ef420d1fec1ca90b7ba5b609ec08fa1fe0cf7d
Lucas Becker <lucasbecker.dev@gmail.com>
2024-06-11 23:46:09 -0400

    added exception catching and findByName()

--- src/main/java/com/coderscampus/assignment14/service/UserService.java
+++ src/main/java/com/coderscampus/assignment14/service/UserService.java
@@ -1,6 +1,9 @@
 package com.coderscampus.assignment14.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.stereotype.Service;
 
 import com.coderscampus.assignment14.domain.User;
@@ -16,6 +19,18 @@ public class UserService {
     }
 
     public User save(User user) {
-        return userRepo.save(user);
+        try {
+            return userRepo.save(user);
+        } catch (IllegalArgumentException e) {
+            System.err.println("Error: user cannot be null\n" + e);
+            return null;
+        } catch (OptimisticLockingFailureException e) {
+            System.err.println(e);
+            return null;
+        }
+    }
+
+    public List<User> findByName(String name) {
+        return userRepo.findByName(name);
     }
 }
