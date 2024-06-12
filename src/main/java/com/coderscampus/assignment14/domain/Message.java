602266a27623ed52388109359f18062d36247276
Lucas Becker <lucasbecker.dev@gmail.com>
2024-06-11 23:44:09 -0400

    add @Builder from Lombok

--- src/main/java/com/coderscampus/assignment14/domain/Message.java
+++ src/main/java/com/coderscampus/assignment14/domain/Message.java
@@ -1,10 +1,12 @@
 package com.coderscampus.assignment14.domain;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Data;
 
 @Entity
 @Data
+@Builder
 public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
