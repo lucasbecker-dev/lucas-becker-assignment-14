8a36f8225e13ae70a2d7b4e4f147c9791b878881
Lucas Becker <lucasbecker.dev@gmail.com>
2024-06-11 23:45:31 -0400

    added exception catching and builder pattern for Message posting

--- src/main/java/com/coderscampus/assignment14/rest/MessageRestController.java
+++ src/main/java/com/coderscampus/assignment14/rest/MessageRestController.java
@@ -6,23 +6,50 @@ import org.springframework.web.bind.annotation.*;
 import com.coderscampus.assignment14.domain.Message;
 import com.coderscampus.assignment14.service.ChannelService;
 import com.coderscampus.assignment14.service.MessageService;
+import com.coderscampus.assignment14.service.UserService;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class MessageRestController {
     private final MessageService messageService;
     private final ChannelService channelService;
+    private final UserService userService;
 
     @Autowired
-    public MessageRestController(MessageService messageService, ChannelService channelService) {
+    public MessageRestController(MessageService messageService, ChannelService channelService,
+            UserService userService) {
         this.messageService = messageService;
         this.channelService = channelService;
+        this.userService = userService;
     }
 
     @PostMapping("/message")
-    public Message createMessage(@RequestBody Message message) {
-        return messageService.save(message);
+    public Message createMessage(@RequestBody Map<String, String> message) {
+        System.out.println(message);
+        Message newMessage = null;
+        try {
+            newMessage = Message
+                    .builder()
+                    .content(message.get("content"))
+                    .user(userService.findByName(message.get("userName")).get(0))
+                    .channel(channelService.findById(Long.parseLong(message.get("channelId"))))
+                    .build();
+            return messageService.save(newMessage);
+        } catch (NumberFormatException e) {
+            System.err.println("Error: invalid channelId: " + message.get("channelId") + '\n' + e);
+            return null;
+        } catch (ClassCastException e) {
+            System.err.println(e);
+            return null;
+        } catch (NullPointerException e) {
+            System.err.println(e);
+            return null;
+        } catch (IndexOutOfBoundsException e) {
+            System.err.println("Error: no existing User with name: " + message.get("userName") + '\n' + e);
+            return null;
+        }
     }
 
     @GetMapping("/channels/{channelId}/messages")
